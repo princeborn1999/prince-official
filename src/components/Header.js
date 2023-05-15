5e8165d80606eee09e06ee25db1892b9ae068963
AaronPrince <princeborn1999@gmail.com>
2023-05-15 17:33:06 +0800

    [ Feature ] anime

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,34 @@
-import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import React,{ useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import FingerprintIcon from "@mui/icons-material/Fingerprint";
 import Switch from "@mui/material/Switch";
-
+import anime from 'animejs';
 function Header() {
+  useEffect(()=>{
+    anime({
+      targets: '.my-nav',
+      translateY: [{ value: -100, duration: 500 },
+                   {value: 0, duration: 800, easing: 'easeOutBounce'}],
+      opacity: [ {value: 0, duration: 500}, {value: 1, duration: 800} ],
+      delay: 500,
+    })
+  },[])
   return (
     <header className="flex p-5">
       <FingerprintIcon arial-label="logo" />
       <div aria-label="nav" className="flex justify-center w-full">
-        <div>
+         <div className="my-nav">
           <Link to="/">
-            <Button>HOME</Button>
+            <Button >HOME</Button>
           </Link>
-          <Link to="/experience">
+          <Link to="/experience" >
             <Button>EXPERIENCE</Button>
           </Link>
-          <Link to="/interest">
+          <Link to="/interest" >
             <Button>Interest</Button>
           </Link>
-          {/* MORE在另一種模式才會出現 */}
+        {/* MORE在另一種模式才會出現 */}
           <Link to="/more">
             <Button>MORE</Button>
           </Link>
--- src/pages/Interest.js
+++ src/pages/Interest.js
@@ -1,14 +1,31 @@
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { useState } from 'react';
 
 export function Interest() {
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Tabs>
-      <Tab label="Body Builder"></Tab>
-      <Tab label="Open Source"></Tab>
-      <Tab label="Manga">
-        <div>Manga</div>
-      </Tab>
-    </Tabs>
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="Interest list">
+            <Tab label="Body Builder" value="1" />
+            <Tab label="Open Source" value="2" />
+            <Tab label="Manga" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
   );
 }

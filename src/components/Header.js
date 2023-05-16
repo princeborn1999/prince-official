b83d9f307b3e2cbbbeb8c89b652caadf7228ea7f
Prince <princeborn1999@gmail.com>
2023-05-17 02:03:57 +0800

    [ Feature ] Profile section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,58 @@
-import React,{ useState, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMode } from "../store/modeActions";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import Switch from "@mui/material/Switch";
-import anime from 'animejs';
+import anime from "animejs";
 function Header() {
   // debugger
   const dispatch = useDispatch();
-  const mode = useSelector(state => state.mode.mode);
+  const mode = useSelector((state) => state.mode.mode);
   const handleModeToggle = () => {
     dispatch(toggleMode());
   };
-  useEffect(()=>{
+  useEffect(() => {
     anime({
-      targets: '.my-nav',
-      translateY: [{ value: -100, duration: 500 },
-                   {value: 0, duration: 800, easing: 'easeOutBounce'}],
-      opacity: [ {value: 0, duration: 500}, {value: 1, duration: 800} ],
+      targets: ".my-nav",
+      translateY: [
+        { value: -100, duration: 500 },
+        { value: 0, duration: 800, easing: "easeOutBounce" },
+      ],
+      opacity: [
+        { value: 0, duration: 500 },
+        { value: 1, duration: 800 },
+      ],
       delay: 500,
-    })
-  },[])
+    });
+  }, []);
   return (
-    <header className="flex p-5">
+    <header className="flex p-5 shadow-md">
       {/* 之後放ICON */}
       <div aria-label="nav" className="flex justify-center w-full">
-         <div className="my-nav">
+        <div className="my-nav">
           <Link to="/">
             <Button>HOME</Button>
           </Link>
-          <Link to="/skill" >
-            <Button>Skill</Button>
+          <Link to="/work">
+            <Button>WORK</Button>
           </Link>
-          <Link to="/experience" >
+          <Link to="/experience">
             <Button>EXPERIENCE</Button>
           </Link>
-          <Link to="/interest" >
+          <Link to="/interest">
             <Button>Interest</Button>
           </Link>
-        {/* MORE在另一種模式才會出現 */}
-          {mode && <Link to="/more" >
-            <Button>MORE</Button>
-          </Link>}
+          {/* MORE在另一種模式才會出現 */}
+          {mode && (
+            <Link to="/more">
+              <Button>MORE</Button>
+            </Link>
+          )}
         </div>
       </div>
       <div>
-        <Switch 
-            checked={mode}
-            onChange={handleModeToggle}
-        />
+        <Switch checked={mode} onChange={handleModeToggle} />
       </div>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ export function Home() {
     <div className="grid grid-cols-10">
       <section id="section-right" className="col-span-8 p-5 pl-10">
         <div className="text-3xl text-white p-5 rounded-2xl font-bold bg-gradient-to-r from-cyan-500 to-blue-500">
-          <p>Three.js</p>
+          <p>Youtube video</p>
           <p>If you don't make changes now,</p>
           <p>when will you make them?</p>
         </div>
       </section>
-      <section id="section-left" className="col-span-2 p-5 pr-10">
-        <div className="bg-gradient-to-r p-5 rounded-lg bg-slate-400">
+      <section id="section-left" className="col-span-2">
+        <div className="bg-gradient-to-r p-10 border">
           <img
             className="rounded-full"
             src={ProfilePicture}
@@ -23,10 +23,14 @@ export function Home() {
           <p>Skill: React</p>
           <p> Angular </p>
           <p>Node</p>
-          <section>
-            <p>Link to my github: ""</p>
-            <p>Link to my CV: ""</p>
-          </section>
+          <p>
+            <a href="https://github.com/princeborn1999">Github</a>
+          </p>
+          <p>
+            <a href="https://docs.google.com/document/d/e/2PACX-1vQhMXbyd7-IKtyViojFdgtrzkBx4ZXlUnC_AIxJQDghBH8tmBa8hWD-AhNLJ9V67-Jj6ty48dyaySlk/pub">
+              CV
+            </a>
+          </p>
         </div>
       </section>
     </div>

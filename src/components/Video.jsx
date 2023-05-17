5b8ae5981a81960a61ce6fc5421ee7d78c4b256b
AaronPrince <princeborn1999@gmail.com>
2023-05-17 15:52:26 +0800

    [ Feature ] Embed youtube video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,52 @@
-import { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+import "videojs-youtube";
+import "video.js/dist/video-js.css";
+
+const initialOptions = {
+    controls: true,
+    fluid: true,
+    controlBar: {
+      volumePanel: {
+        inline: false
+      }
+    }
+};
+
+const videoJsOptions = {
+sources: [
+    {
+    type: "video/youtube", //important
+    src: "https://www.youtube.com/watch?v=GlhV-OKHecI"
+    }
+    ]
+};
+  
 
 function Video(props){
-  
+    const videoNode = useRef(null);
+    const player = useRef(null);
+    const initialized = useRef(false);
+    useEffect(() => {
+        if (videoNode.current && !initialized.current) {
+          initialized.current = true; //prevent duplicate initialization
+          player.current = videojs(videoNode.current, {
+            ...initialOptions,
+            ...videoJsOptions
+          }).ready(function () {
+            console.log("Player Ready");
+          });
+        }
+        //clear up player on dismount
+        return () => {
+          if (player.current) {
+            player.current.dispose();
+          }
+        };
+      }, []);
     return(
         <div>
-            <p>
-                <p></p>
-            </p>
+            <video ref={videoNode} className="video-js" />
         </div>
     )
 }
